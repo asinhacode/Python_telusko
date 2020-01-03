0ffd24708071eb2c6ae17fab6562f5b9bd6b62d2
Anupam Sinha <57113053+asinhacode@users.noreply.github.com>
2020-01-03 04:45:50 -0500

    multiple declaration

---
+++ Introduction/Operator.py
@@ -0,0 +1,21 @@
+x = 3
+y = 6
+
+print(x * y)
+print(y % x)
+
+# compound operator
+x *= 7
+print(x)
+
+# multiple declaration
+r, a, t = 18, 1, 20
+print(r)
+print(a)
+print(t)
+
+print(r > a)
+print(r > a and t < a)
+
+# conversion from decimal to binary
+print(bin(25))
