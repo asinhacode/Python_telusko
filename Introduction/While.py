85f7189e6efe09725c13058cbaf06d735007f5be
Anupam Sinha <57113053+asinhacode@users.noreply.github.com>
2020-01-05 08:30:27 -0500

    while, nested and end

---
+++ Introduction/While.py
@@ -0,0 +1,15 @@
+i = 1;
+
+while i < 100:
+    if( i % 15 == 0):
+        print(i)
+    i += 1
+
+# nested
+j = 0
+while j < 5:
+    k = 0.0
+    while k < 0.5:
+        print(j + k)
+        k += 0.1
+    j += 1
